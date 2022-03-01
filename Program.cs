6d60321d26f6f971100dd533c5370668e83ba653
Daniel Kristensen <DalleDonDalle@hotmail.com>
2022-03-01 14:00:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using WindowManipulator.Library;
+
+namespace WindowManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = new Settings();
+            setting.SetFocusWindowSettings(1850, 900);
+            foreach (var item in Process.GetProcesses().Where(x => x.ProcessName == "Notepad"))
+            {
+                setting.AddProcess(item);
+            }
+
+            while(true)
+            {
+                setting.UpdateWindows();
+            }
+        }
+    }
+}
